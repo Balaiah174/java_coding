0e5719097fd8602442f660c1bb66d70635c9eff8
Balaiah <thupakulabalaiah352@gmail.com>
2025-02-16 16:27:34 +0530

    new

---
+++ classcoding/src/numbers/Buzznumber.java
@@ -0,0 +1,13 @@
+package numbers;
+
+public class Buzznumber {
+public static void main(String[] args) {
+	int num=63;
+	if(num%10==7||num%7==0) {
+		System.out.println("is a buzz number");
+	}
+	else {
+		System.out.println("is not a buzz number");
+	}
+}
+}
--- classcoding/src/numbers/LCM.java
+++ classcoding/src/numbers/LCM.java
@@ -13,7 +13,7 @@ public class LCM {
 		
 		int temp1 = product / a;
 		}
-		System.out.println();
+		System.out.println(product);
 	}
 	
 }
